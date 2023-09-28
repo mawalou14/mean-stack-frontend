31ab6a184e150ad851bc2506cd703267fa6d7268
mawalou14 <awaloum213@gmail.com>
2023-09-29 00:01:52 +0100

    Design created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeFormComponent } from './employee/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styles: [
+  ]
+})
+export class EmployeeComponent {
+
+}
